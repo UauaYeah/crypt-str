80d164f232286c1b76b7374a5035a909263c4900
ika286yade <111626537+UauaYeah@users.noreply.github.com>
2024-08-30 16:49:14 +0900

    Update cryptstr.hpp

--- cryptstr.hpp
+++ cryptstr.hpp
@@ -13,14 +13,14 @@ namespace cryptstr {
         static constexpr int hours = (time_str[0] - '0') * 10 + (time_str[1] - '0');
         static constexpr int minutes = (time_str[3] - '0') * 10 + (time_str[4] - '0');
         static constexpr int seconds = (time_str[6] - '0') * 10 + (time_str[7] - '0');
-        static constexpr int randTime = hours * 3600 + minutes * 60 + seconds;
+        static constexpr int randTime = hours * minutes * seconds;
     public:
 
         static constexpr unsigned long long multiplier = 964136223846793005ULL;
         static constexpr unsigned long long increment = 1ULL;
-        static constexpr int randomized = ((1234567890ULL * multiplier + randTime + increment) % (1ULL << 63)) % 999999999999999999;
+        static constexpr int randomized = ((123456789ULL * multiplier + randTime + increment) % (1ULL << 63)) % 999999999999999999;
 
-        static constexpr int rkey = randomized % randTime;
+        static constexpr int rkey = (randomized % randTime) * randTime;
         static constexpr size_t max_len = 256;
 
         template <size_t N>
@@ -34,7 +34,6 @@ namespace cryptstr {
 
         template <size_t N>
         static std::string xordecrypt(const std::array<char, N>& enc) noexcept {
-            std::cout << rkey << std::endl;
             std::string r;
             for (size_t i = 0; i < N && enc[i] != '\0'; ++i) {
                 r.push_back(enc[i] ^ rkey);
