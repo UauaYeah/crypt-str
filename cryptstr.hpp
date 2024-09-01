551a31b70870f18385435ecdbd6a18ac67c11ba5
ika286yade <111626537+UauaYeah@users.noreply.github.com>
2024-09-01 18:59:53 +0900

    fix

--- cryptstr.hpp
+++ cryptstr.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <array>
 #include <cstddef>
+#include <type_traits>
+#include <iostream>
 
 #define finline __forceinline
 
@@ -13,48 +15,38 @@ namespace cryptstr {
         static constexpr int hours = (time_str[0] - '0') * 10 + (time_str[1] - '0');
         static constexpr int minutes = (time_str[3] - '0') * 10 + (time_str[4] - '0');
         static constexpr int seconds = (time_str[6] - '0') * 10 + (time_str[7] - '0');
-        static constexpr int randTime = hours * minutes * seconds;
-    public:
+        static constexpr int randTime = (hours * minutes * seconds);
 
         static constexpr unsigned long long a = 16264525ULL;
         static constexpr unsigned long long c = 8013904223ULL;
-        static constexpr unsigned long long m = 0xFFFFFFFF;
+        static constexpr unsigned long long m = 0xFFFFFFFFF;
+        static constexpr int rkey = ((((a * randTime + c) % m) % 3ULL)) * 0xFF;
 
-        static constexpr int rkey = (((a * randTime + c) % m) % randTime) * randTime;
-        static constexpr size_t max_len = 256;
-
-        template <size_t N>
-        static constexpr std::array<char, N> xorencrypt(const char(&str)[N]) noexcept {
-            std::array<char, N> result{};
+        template <typename T, size_t N>
+        static constexpr std::array<T, N> xorencrypt(const T(&str)[N]) noexcept {
+            std::array<T, N> result{};
             for (size_t i = 0; i < N - 1 && str[i] != '\0'; ++i) {
                 result[i] = str[i] ^ rkey;
             }
             return result;
         }
-        template <size_t N>
-        static std::array<char, N - 1> xordecrypt(const std::array<char, N>& enc) noexcept {
-            std::array<char, N - 1> r = std::array<char, N - 1>();
-            for (size_t i = 0; i < N && enc[i] != '\0'; ++i) {
-                r[i] = enc[i] ^ rkey;
-            }
-            return r;
-        }
 
-        template <size_t N>
-        static const char* decryptToCString(const std::array<char, N>& enc) noexcept {
-            static std::array<char, N - 1> decrypted = xordecrypt(enc);
-            return decrypted.data();
-        }
-    public:
-        template <size_t N>
-        static std::string get(const char(&str)[N]) noexcept {
-            auto encrypted = xorencrypt(str);
-            return xordecrypt(encrypted);
+        template <typename T, size_t N>
+        static constexpr std::array<T, N> xordecrypt(const std::array<T, N>& enc) noexcept {
+            std::array<T, N> result{};
+            for (size_t i = 0; i < N - 1 && enc[i] != '\0'; ++i) {
+                result[i] = enc[i] ^ rkey;
+            }
+            return result;
         }
     };
 }
 
-#define crypt(str) []() noexcept { \
-    constexpr auto encrypted = ::cryptstr::crypt::xorencrypt(str); \
-    return ::cryptstr::crypt::decryptToCString(encrypted); \
-}()
+#define crypt_(str) ([]() noexcept { \
+    using str_type = std::remove_cv_t<std::remove_pointer_t<decltype(str)>>; \
+    constexpr size_t size = sizeof(str) / sizeof(str[0]); \
+    using crypt_type = ::cryptstr::crypt; \
+    constexpr auto encrypted = crypt_type::xorencrypt(str); \
+    return crypt_type::xordecrypt(encrypted).data(); \
+}())
+#define crypt(str) (crypt_(str))
